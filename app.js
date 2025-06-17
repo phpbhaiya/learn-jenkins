033c33003f5f91df4bf3c4856f034b19dfb7daa2
Subhadeep <phpbhaiya@gmail.com>
2025-06-17 21:02:50 +0530

    pehla commit

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello, Jenkins!');
+});
+
+if (require.main === module) {
+  const PORT = process.env.PORT || 6666;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
